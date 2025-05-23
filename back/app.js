a81382b04e6730b4cdaa0340ba28091694146f99
yuyoung <hyyy8810@gmail.com>
2025-05-23 20:20:18 +0900

    fix: front connecting error

--- back/app.js
+++ back/app.js
@@ -9,7 +9,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+//app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true
+  }));
 app.use(express.json());
 
 app.use('/api/users', userRoutes);
