a9c9e979f6fa9b6869c978063b74ed03c56b1ee7
yuyoung <hyyy8810@gmail.com>
2025-05-23 20:46:21 +0900

    fix: changed cors lines in app.js

--- back/app.js
+++ back/app.js
@@ -10,8 +10,14 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 //app.use(cors());
+// app.use(cors({
+//     origin: 'http://localhost:3000',
+//     credentials: true
+//   }));
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'http://localhost:3000',  // 프론트 주소
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
   }));
 app.use(express.json());
