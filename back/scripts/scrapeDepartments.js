3c512eec2feb17768a80ce9414e8ebbfefd8d2ee
yuyoung <hyyy8810@gmail.com>
2025-05-24 13:19:48 +0900

    fix: changed departments scrapping from korean to Eng

--- back/scripts/scrapeDepartments.js
+++ back/scripts/scrapeDepartments.js
@@ -8,6 +8,10 @@ const TARGET_URL = 'https://for-s.seoultech.ac.kr/html/pub/schedule.jsp';
   const browser = await puppeteer.launch({ headless: 'new' });
   const page = await browser.newPage();
   await page.goto(TARGET_URL, { waitUntil: 'networkidle2' });
+    // 언어를 English로 설정 (언어 선택 셀렉트박스)
+    await page.select('#cbo_lang', 'en');
+    //await page.waitForTimeout(500); // 언어가 바뀌는 걸 기다림
+    await new Promise(resolve => setTimeout(resolve, 500));
 
   // Select box 안의 <option> 요소 추출
   const departments = await page.evaluate(() => {
