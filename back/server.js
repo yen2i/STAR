55e7acea7906421c8b0f941634af455b6c7ccfaf
yuyoung <hyyy8810@gmail.com>
2025-06-14 02:41:17 +0900

    fix: changed server success message

--- back/server.js
+++ back/server.js
@@ -6,12 +6,12 @@ const PORT = process.env.PORT || 8080;
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-  }).then(() => console.log('✅ Successfully connected to MongoDB'))
-    .catch(err => console.error('❌ Failed to connect to MongoDB:', err));
+  }).then(() => console.log('➡️ Successfully connected to MongoDB'))
+    .catch(err => console.error('Failed to connect to MongoDB:', err));
   
   
 
 // Start server
 app.listen(PORT, () => {
-    console.log(`🚀 Server is running on http://localhost:${PORT}`);
+    console.log(`➡️ Server is running on http://localhost:${PORT}`);
   });
